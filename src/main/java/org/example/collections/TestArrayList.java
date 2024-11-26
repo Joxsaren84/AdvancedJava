9796c0a9ce6a7f6a64633dc66e168aef0484283a
Keypa <42-18@mail.ru>
2024-11-26 10:58:08 +1000

    Add first lecture of ArrayList

---
+++ src/main/java/org/example/collections/TestArrayList.java
@@ -0,0 +1,31 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestArrayList {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++){
+            list.add(i);
+        }
+
+        System.out.println(list);
+        System.out.println("Size of list: " + list.size());
+        System.out.println("First element: " + list.get(0));
+        System.out.println("Last element: " + list.get(list.size() - 1));
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        list.remove(5);
+
+        for (Integer i : list){
+            System.out.println(i);
+        }
+
+    }
+}
